e22dad20092e66a2dfad171f9357ed43c815dac4
Kisaragi Hiu <mail@kisaragi-hiu.com>
2023-05-16 18:24:03 +0900

    compress when building
    In Chromium with Slow 3G throttling:
    
    - without this: 20 seconds
    - with this: 7 seconds

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -1,11 +1,13 @@
-import adapter from "@sveltejs/adapter-static";
+import adapterStatic from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/kit/vite";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
   kit: {
-    adapter: adapter(),
+    adapter: adapterStatic({
+      precompress: true,
+    }),
   },
 };
 
