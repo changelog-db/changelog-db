70e0cb7e90c389391e19b6a7657adc576c9eb05f
Kisaragi Hiu <mail@kisaragi-hiu.com>
2023-06-07 04:05:43 +0900

    Scroll to top after switching pages

--- web/src/routes/Pages.svelte
+++ web/src/routes/Pages.svelte
@@ -2,6 +2,16 @@
   export let maxPage: number;
   import clsx from "clsx";
   import { page } from "$lib/stores";
+
+  function top() {
+    if (window) {
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+      });
+    }
+  }
 </script>
 
 <div id="pagination" class="join mb-4 flex justify-center">
@@ -9,15 +19,21 @@
     class={clsx("join-item btn", $page <= 1 && "btn-disabled")}
     on:click={() => {
       $page = Math.max($page - 1, 1);
+      top();
     }}>«</button
   >
-  <button class="join-item btn w-[8ch]" on:click={() => ($page = 1)}
-    >{$page}/{maxPage}</button
+  <button
+    class="join-item btn w-[8ch]"
+    on:click={() => {
+      $page = 1;
+      top();
+    }}>{$page}/{maxPage}</button
   >
   <button
     class={clsx("join-item btn", $page >= maxPage && "btn-disabled")}
     on:click={() => {
       $page = Math.min($page + 1, maxPage);
+      top();
     }}>»</button
   >
 </div>
