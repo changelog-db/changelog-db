cc6cb004059f305f0cddef993c87a8a737650664
Kisaragi Hiu <mail@kisaragi-hiu.com>
2023-06-15 02:53:08 +0900

    yaml-ts-mode

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,5 +2,6 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((yaml-mode . ((eval . (progn (load (expand-file-name "cmds.el")) (ChangelogDB:dev-setup)))))
+ (yaml-ts-mode . ((eval . (progn (load (expand-file-name "cmds.el")) (ChangelogDB:dev-setup)))))
  (markdown-mode . ((eval . (progn (load (expand-file-name "cmds.el")) (ChangelogDB:dev-setup))))))
 
