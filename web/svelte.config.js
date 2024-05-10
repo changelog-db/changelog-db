4e2ef60b2c2ea5d21aaefd3db6f86a1f52e54571
Kisaragi Hiu <mail@kisaragi-hiu.com>
2024-05-10 20:54:39 +0900

    suppress suprious error from svelte-language-server

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -4,16 +4,28 @@ import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: [
-    vitePreprocess(),
-    mdsvex({
-      extensions: [".md"],
-      // We have to specify the base path so that svelte language server can
-      // find it in the right place. It falls back to repo root, but we're not
-      // at repo root here.
-      layout: `${import.meta.dirname}/src/lib/MarkdownLayout.svelte`,
-    }),
-  ],
+  preprocess:
+    // When Svelte reads this file, the layout is found at the right place.
+    // However, when svelte-language-server reads this file, it tries the path
+    // relative to the file being edited, falls back to *repo root* if not
+    // found, then errors if it's still not found. Since we're not at repo root,
+    // it ends up always reporting an error. It should fall back to the location
+    // of this file instead, but... it just doesn't.
+    //
+    // This hack suppresses the spurious error.
+    (() => {
+      try {
+        return [
+          vitePreprocess(),
+          mdsvex({
+            extensions: [".md"],
+            layout: "./src/lib/MarkdownLayout.svelte",
+          }),
+        ];
+      } catch {
+        return;
+      }
+    })(),
   extensions: [".svelte", ".md"],
   kit: {
     adapter: adapterNetlify({
