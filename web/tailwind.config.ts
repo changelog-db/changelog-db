2720c6f4c46857fcfac8cc2245557878d4e8c6eb
Kisaragi Hiu <mail@kisaragi-hiu.com>
2024-05-10 21:14:36 +0900

    Try another set of themes

--- web/tailwind.config.ts
+++ web/tailwind.config.ts
@@ -6,6 +6,7 @@ const cjkFallbackSans = [
   "Meiryo",
   "Malgun Gothic",
 ];
+
 export default {
   content: ["./src/**/*.{html,js,ts,svelte}"],
 
@@ -22,25 +23,6 @@ export default {
     // output of prettier-plugin-tailwindcss.
     // Surely it should be printing to stderr?
     logs: false,
-    themes: [
-      "cupcake",
-      {
-        // Dracula, but with neutral set to dracula's "neutral-content".
-        // In builtin light themes neutral would be dark, but in
-        // builtin dark themes neutral is still dark.
-        dark: {
-          primary: "#ff79c6",
-          secondary: "#bd93f9",
-          accent: "#ffb86c",
-          neutral: "#d6d7db",
-          "base-100": "#282a36",
-          "base-content": "#f8f8f2",
-          info: "#8be9fd",
-          success: "#50fa7b",
-          warning: "#f1fa8c",
-          error: "#ff5555",
-        },
-      },
-    ],
+    themes: ["emerald", "night"],
   },
 };
