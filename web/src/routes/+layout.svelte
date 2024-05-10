a4f0cb1a9f06654ad386536ce4f3838fe1b5cafe
Kisaragi Hiu <mail@kisaragi-hiu.com>
2024-05-10 18:44:17 +0900

    Split description and list

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -1,7 +1,25 @@
-<script>
+<script lang="ts">
   import "../src.css";
+  import { page } from "$app/stores";
+  const tabs = [
+    ["/", "Home"],
+    ["/changelogs", "Changelogs"],
+  ];
 </script>
 
 <main class="mx-auto my-16 w-[95%] max-w-[40rem]">
+  <div class="prose">
+    <h1>ChangelogDB</h1>
+    <div role="tablist" class="tabs tabs-bordered tabs-lg">
+      {#each tabs as [href, title]}
+        <a
+          {href}
+          role="tab"
+          class={href === $page.url.pathname ? "tab tab-active" : "tab"}
+          >{title}</a
+        >
+      {/each}
+    </div>
+  </div>
   <slot />
 </main>
--- web/src/routes/changelogs/+page.svelte
+++ web/src/routes/changelogs/+page.svelte
@@ -136,69 +136,10 @@
 
 <svelte:head>
   <!-- Primary Meta Tags -->
-  <title>ChangelogDB</title>
-  <meta name="title" content="ChangelogDB" />
-  <meta
-    name="description"
-    content="An npm registry package changelog index. Manually collected, best-effort, hopefully saves some time."
-  />
-
-  <!-- Open Graph / Facebook -->
-  <meta property="og:type" content="website" />
-  <meta property="og:url" content="https://changelog-db.netlify.app/" />
-  <meta
-    property="og:image"
-    content="https://changelog-db.netlify.app/og-image.png"
-  />
-  <meta property="og:title" content="ChangelogDB" />
-  <meta
-    property="og:description"
-    content="An npm registry package changelog index. Manually collected, best-effort, hopefully saves some time."
-  />
-
-  <!-- Twitter -->
-  <meta property="twitter:creator" content="@kisaragi_hiu" />
-  <meta property="twitter:card" content="summary_large_image" />
-  <meta property="twitter:url" content="https://changelog-db.netlify.app/" />
-  <meta
-    property="twitter:image"
-    content="https://changelog-db.netlify.app/og-image.png"
-  />
-  <meta property="twitter:title" content="ChangelogDB" />
-  <meta
-    property="twitter:description"
-    content="An npm registry package changelog index. Manually collected, best-effort, hopefully saves some time."
-  />
+  <title>Packages | ChangelogDB</title>
+  <meta name="title" content="Packages | ChangelogDB" />
 </svelte:head>
 
-<div class="prose">
-  <h1>ChangelogDB</h1>
-  <p>The missing changelog field in package.json.</p>
-  <p>
-    PyPI has it, and it's great! The JavaScript ecosystem should have it as
-    well.
-  </p>
-  <p>
-    For now, here's a package changelog index. <a
-      href="https://github.com/changelog-db/changelog-db/blob/-/changelog-db.data"
-      target="_blank">Manually collected</a
-    >
-    (semi-automated with Emacs), best-effort, hopefully saves some time.
-  </p>
-</div>
-<p class="prose mt-4">
-  A project by <a href="https://twitter.com/kisaragi_hiu" target="_blank"
-    >@kisaragi_hiu</a
-  >
-  —
-  <a href="https://www.buymeacoffee.com/kisaragihiu" target="_blank"
-    >help pay for my rent</a
-  >
-</p>
-<p class="prose my-4 text-xs">
-  <a href="https://github.com/changelog-db/changelog-db">Built with</a>{" "}
-  Tailwind CSS, daisyUI, and SvelteKit.
-</p>
 <input
   type="search"
   placeholder={browser ? `Search ${data.length} entries` : "Loading"}
