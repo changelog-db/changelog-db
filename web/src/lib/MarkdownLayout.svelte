942e20a5a26c2b81ad3195555d996d904752714c
Kisaragi Hiu <mail@kisaragi-hiu.com>
2024-05-10 20:07:51 +0900

    mdsvex

---
+++ web/src/lib/MarkdownLayout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  export let title: string;
+  export let desc: string;
+</script>
+
+<svelte:head>
+  <title>{title} | ChangelogDB</title>
+  <meta name="title" content="{title} | ChangelogDB" />
+  <meta name="description" content={desc} />
+</svelte:head>
+
+<div class="prose mb-8">
+  <slot />
+</div>
--- web/svelte.config.js
+++ web/svelte.config.js
@@ -1,9 +1,14 @@
 import adapterNetlify from "@sveltejs/adapter-netlify";
+import { mdsvex } from "mdsvex";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: vitePreprocess(),
+  preprocess: [
+    vitePreprocess(),
+    mdsvex({ extensions: [".md"], layout: "./src/lib/MarkdownLayout.svelte" }),
+  ],
+  extensions: [".svelte", ".md"],
   kit: {
     adapter: adapterNetlify({
       edge: true,
